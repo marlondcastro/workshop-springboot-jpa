3c4fe9a064eb7bcfe999a092c25998c2b3561d23
Marlon Castro <marlondc@ciandt.com>
2026-03-27 15:57:13 -0300

    Order instant Parte 1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,6 +3,8 @@ package com.educandoweb.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,8 +20,10 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
-    public User(){
+    @OneToMany(mappedBy = "client")
+     private List<Order> orders = new ArrayList<>();
 
+    public User(){
     }
 
     public User(Long id, String name, String email, String phone, String password) {
@@ -68,6 +72,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
